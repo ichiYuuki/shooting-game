7f517797fdd67339ffcd78f364599696956fa1e8
ichiYuuki <mrofushi@gmail.com>
2016-02-20 18:04:49 +0900

    2/20学校にて

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	Spaceship spaceship;
+	IEnumerator Start(){
+
+		spaceship = GetComponent<Spaceship>();
+
+		while (true) {
+
+			spaceship.Shot (transform);
+			yield return new WaitForSeconds(spaceship.shotDelay);
+		}
+	}
+
+	void Update () {
+		float x = Input.GetAxisRaw ("Horizontal");
+
+		float y = Input.GetAxisRaw ("Vertical");
+
+		Vector2 direction = new Vector2 (x, y).normalized;
+
+		spaceship.Move (direction);
+	}
+
+	void OnTriggerEnter2D(Collider2D c){
+		Destroy (c.gameObject);
+
+		spaceship.Explosion ();
+
+		Destroy (gameObject);
+	}
+}
